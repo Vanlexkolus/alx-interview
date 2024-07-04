20ee21c02f224a4f8d38e34701edc15872327c79
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-07-05 00:13:19 +0100

    done with the project

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,14 +6,14 @@ def canUnlockAll(boxes):
     """
     n = len(boxes)
     unlocked = set()
-    stack = [0]  # Start with the first box
+    stack = [0]
 
     while stack:
         current = stack.pop()
         if current not in unlocked:
             unlocked.add(current)
             for key in boxes[current]:
-                if key < n:  # Ensure the key corresponds to a valid box
+                if key < n:
                     stack.append(key)
 
     return len(unlocked) == n
@@ -22,10 +22,10 @@ def canUnlockAll(boxes):
 # Test cases
 if __name__ == "__main__":
     boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # True
+    print(canUnlockAll(boxes))
 
     boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # True
+    print(canUnlockAll(boxes))
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # False
+    print(canUnlockAll(boxes))
